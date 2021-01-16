6ad630d74885cecaf6ca540119a2639a7b3ae3ce
wox080xow <b04302127@g.ntu.edu.tw>
2021-01-16 17:25:02 +0800

    localhost with virtualenv of test

--- start-all-dashes.sh
+++ start-all-dashes.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-cd
+# for Ubuntu 20.04 Amazon-EC2
+#cd 
+#cd dash_AreaPredict
+#gunicorn -w 1 -b 0.0.0.0:8052 dash_AreaPredict:application --daemon
+
+#cd
+#cd dash_CasePredict
+#gunicorn -w 1 -b 0.0.0.0:8053 dash_CasePredict:application --daemon
+
+#cd
+#cd dash_Statistics
+#gunicorn -w 1 -b 0.0.0.0:8054 dash_Statistics:application --daemon
+
+# for Mac OS X localhost
 cd dash_AreaPredict
-gunicorn -w 1 -b 0.0.0.0:8052 dash_AreaPredict:application --daemon
+gunicorn -w 1 -b 127.0.0.1:8052 dash_AreaPredict:application --daemon
 
-cd
+cd ..
 cd dash_CasePredict
-gunicorn -w 1 -b 0.0.0.0:8053 dash_CasePredict:application --daemon
+gunicorn -w 1 -b 127.0.0.1:8053 dash_CasePredict:application --daemon
 
-cd
+cd ..
 cd dash_Statistics
-gunicorn -w 1 -b 0.0.0.0:8054 dash_Statistics:application --daemon
+gunicorn -w 1 -b 127.0.0.1:8054 dash_Statistics:application --daemon
 
+echo 'All Dash Apps start!'
--- stop-all-dashes.sh
+++ stop-all-dashes.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-dashPID=$( lsof -nPi | grep 805 | cut -d' ' -f 2 )
-kill -9 $dashPID
+# for Ubuntu 20.04
+#dashPID=$( lsof -nPi | grep 805 | cut -d' ' -f 2 )
+#kill -9 $dashPID
+
+# for Mac OS X
+dashPID=$(lsof -nPi | grep 805 | sed "s/    / /g" | cut -d' ' -f2)
+kill -s $dashPID
+echo $dashPID
+echo 'All Dash Apps stoped.'
